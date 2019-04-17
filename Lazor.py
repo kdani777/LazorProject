b026d8eda8e66c68df9ae284605813d1f843301c
ssalen1 <47227356+ssalen1@users.noreply.github.com>
2019-04-16 21:55:11 -0400

    Finally!! Solved all files

--- Lazor.py
+++ Lazor.py
@@ -1,12 +1,10 @@
 '''
-This file is our first attempt at the lazor project. 
-
-**Functions**
-	
-	read_map:
-		This function reads the map .bff file and parses the grid, what blocks are available, 
-		what way the lazor is pointing, and the positions the lazor has to hit
+Authors: Kunal Dani, Marina Morrow, Shyanne Salen
+Last Modified: April 7th, 2019
 '''
+from collections import Counter
+import random
+import copy
 
 def read_map(filename):
 	'''
@@ -31,72 +29,269 @@ def read_map(filename):
 			The positions the lazor must intersect
 	'''
 	data = open(filename, 'r').read()
-
 	split_strings = data.strip().split('\n')
 	grid = []
 	in_grid = False
+	positions_to_intersect = []
+	initial_lazor = []
+	number_of_opaque_blocks = 0
+	number_of_reflect_blocks = 0
+	number_of_refract_blocks = 0
 	for line in split_strings:
 		if not in_grid:
 			if line.startswith('GRID START'):
 				in_grid = True
+			elif line.startswith("A"): 
+				number_of_reflect_blocks = int(line.strip().split()[1])
+			elif line.startswith("B"):
+				number_of_opaque_blocks = int(line.strip().split()[1])
+			elif line.startswith("C"):
+				number_of_refract_blocks = int(line.strip().split()[1])
+			elif line.startswith("L"):
+				initial_lazor.append(line.strip().split()[1:])
+			elif line.startswith("P"):
+				positions_to_intersect.append(tuple(map(float, line.strip().split()[1:])))
+			else:
+				continue
 		elif line.startswith('GRID STOP'):
 				in_grid = False
 		else:
 			grid.append(line.strip().split())
+	return grid, number_of_reflect_blocks, number_of_refract_blocks, number_of_opaque_blocks, initial_lazor, positions_to_intersect
 
-	positions_to_intersect = []
-	number_of_opaque_blocks = 0
-	number_of_reflect_blocks = 0
-	number_of_refract_blocks = 0
+class block(object):
+	def __init__(self, blocktype):
+		if blocktype == "A": # reflective block
+			self.blocktype = "A"
+			self.transmit = False
+			self.reflect = True
+			self.position = []
+		if blocktype == "B": #opaque
+			self.blocktype = "B"
+			self.transmit = False
+			self.reflect = False
+			self.position = []
+		if  blocktype == "C": #refract
+			self.blocktype = "C"
+			self.transmit = True
+			self.reflect = True
+			self.position = []
 
-	for line in split_strings:
-		if line.startswith("A"): 
-			number_of_reflect_blocks = line.strip().split()[1]
-		elif line.startswith("B"):
-			number_of_opaque_blocks = line.strip().split()[1]
-		elif line.startswith("C"):
-			number_of_refract_blocks = line.strip().split()[1]
-		elif line.startswith("L"):
-			initial_lazor = line.strip().split()[1:]
-		elif line.startswith("P"):
-			positions_to_intersect.append(line.strip().split()[1:])
-		else:
-			continue
-	return grid, number_of_refract_blocks, number_of_refract_blocks, number_of_opaque_blocks, initial_lazor, positions_to_intersect
+	def hit_block(self,x,y,vx,vy,vertical_wall,horizontal_wall):
+		lazors = []
+		lazors_split = []
+		transmits = False
+		reflect = False
+		if self.reflect:
+			reflect = True
+			if vertical_wall:
+				new_vx = -1.0*vx
+				new_vy = vy
+			if horizontal_wall:
+				new_vx = vx
+				new_vy = -1.0*vy
+			lazors = lazor(x, y, new_vx, new_vy)
+		if self.transmit:
+			lazors_split = lazor(x, y, vx, vy)
+			transmits = True
+			lazors_split.move()
+		return lazors, lazors_split, transmits, reflect
 
-class blocks(object):
-	def __init__(self):
-		self.size = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-		self.grid_value = "A"
+class lazor:
+	def __init__(self, x, y, vx, vy):
+		self.x = x
+		self.y = y
+		self.vx = vx
+		self.vy = vy
+	def move(self):
+		self.x += self.vx
+		self.y += self.vy
 
-def create_refract_block():
-	refract_block = blocks()
-	refract_block.grid_value = "C"
-	return refract_block
+#i think all the defs have to be within class
+def block_wall(x,y,block_position,vx,vy):
+	'''
+	Determines if the lazor hits a block wall and if the wall is horizontal or vertical
+	'''
+	horizontal_wall = False
+	vertical_wall = False 
+	block_x = [i[0] for i in block_position]
+	block_y = [i[1] for i in block_position]
+	if x in block_x and y in block_y:
+		inside_block = lazor(x,y,vx,vy)
+		inside_block.move()
+		if inside_block.x in block_x and inside_block.y in block_y:
+			if x % 2 == 0 and y % 2 != 0:
+				vertical_wall = True
+			if y % 2 == 0 and x % 2 != 0:
+				horizontal_wall = True
+	return vertical_wall, horizontal_wall
 
-def create_opaque_block():
-	opaque_block = blocks()
-	opaque_block.grid_value = "B"
-	return opaque_block
+def goalcheck(lazor_path, positions_to_intersect):
+	goal_met = False
+	if set(positions_to_intersect).issubset(lazor_path):
+		goal_met = True
+	return goal_met
+
+def overlap(lazor_path, blocks, first_pass): 
+	overlap = False
+	if first_pass == False:
+		if lazor_path[-2] == lazor_path[0]:
+			if lazor_path[-1] == lazor_path[1]:
+				x = lazor_path[-1][0]
+				y = lazor_path[-1][1]
+				overlap = True
+	return overlap
+
+def place_block(grid, number_of_reflect_blocks, number_of_refract_blocks, number_of_opaque_blocks):
+	position_in_grid = []
+	position_in_new_grid = []
+	blocks = []
+	new_grid = copy.deepcopy(grid)
+	for i,line in enumerate(grid):
+		for j,value in enumerate(line):
+			if value == 'o':
+				position_in_grid.append((j, i))
+	for a in range(number_of_reflect_blocks):
+		blocks.append(block("A"))
+		where_in_blocks = len(blocks)-1
+		position = random.choice(position_in_grid)
+		new_grid[position[1]][position[0]] = "A"
+		position_in_grid.remove(position)
+		for h in range(0, 3):
+			for k in range(0,3):
+				position_in_new_grid.append((2*position[0]+h,2*position[1]+k))
+		blocks[where_in_blocks].position = position_in_new_grid
+		position_in_new_grid = []
+	for b in range(number_of_opaque_blocks):
+		blocks.append(block("B"))
+		where_in_blocks = len(blocks)-1
+		position = random.choice(position_in_grid)
+		new_grid[position[1]][position[0]] = "B"
+		position_in_grid.remove(position)
+		for h in range(0, 3):
+			for k in range(0,3):
+				position_in_new_grid.append((2*position[0]+h,2*position[1]+k))
+		blocks[where_in_blocks].position = position_in_new_grid
+		position_in_new_grid = []
+	for c in range(number_of_refract_blocks):
+		blocks.append(block("C"))
+		where_in_blocks = len(blocks)-1
+		position = random.choice(position_in_grid)
+		new_grid[position[1]][position[0]] = "C"
+		position_in_grid.remove(position)
+		for h in range(0, 3):
+			for k in range(0,3):
+				position_in_new_grid.append((2*position[0]+h,2*position[1]+k))
+		blocks[where_in_blocks].position = position_in_new_grid
+		position_in_new_grid = []
+	return blocks, new_grid
+
+def stationary_blocks(grid):
+	'''
+	Creates objects for the stationary blocks
+	'''
+	position_in_grid = []
+	position_in_new_grid = []
+	blocks = []
+	for i,line in enumerate(grid):
+		for j,value in enumerate(line):
+			if value != 'o':
+				if value != 'x':
+					position_in_grid.append((j, i))
+					blocks.append(block(value))
+	for a in position_in_grid:
+		for h in range(0, 3):
+			for k in range(0,3):
+				position_in_new_grid.append((2*a[0]+h,2*a[1]+k))
+		blocks[position_in_grid.index(a)].position = position_in_new_grid
+		position_in_new_grid = []
+	return blocks
+
+def lazors_in_grid(grid, lazors, a):
+	if lazors[a].x >= 0.0 and lazors[a].y >= 0.0:
+		if lazors[a].x <= float(2*len(grid)) and lazors[a].y <= float(2*len(grid)):
+			return True
 
 def solve_grid(filename):
-	grid, number_of_refract_blocks, number_of_refract_blocks, number_of_opaque_blocks, initial_lazor, positions_to_intersect = read_map(filename)
-	print grid
-	new = []
-	new_grid = []
-	for i in range(0, 9):
-		new_grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-	for i in grid:
-		for j in i:
-			if j != "o":
-				print 0+3*i.index(j)
-				for k in range(0, 3):
-					for h in range(0, 3):
-						new_grid[k+3*i.index(j)][h+3*i.index(j)] = 1
-			else:
-				continue
-	
+	grid, number_of_reflect_blocks, number_of_refract_blocks, number_of_opaque_blocks, initial_lazor, positions_to_intersect = read_map(filename)
+	lazor_path_initial = []
+	initial_lazors = []
+	for initial in initial_lazor:
+		x = float(initial[0])
+		y = float(initial[1])
+		vx = float(initial[2])
+		vy = float(initial[3])
+		lazor_path_initial.append((x,y))
+		initial_lazors.append(lazor(x,y,vx,vy))
+	goal_met = False
+	while not goal_met:
+		lazor_path = copy.deepcopy(lazor_path_initial)
+		lazor_1 = []
+		lazor_2 = []
+		lazors = copy.deepcopy(initial_lazors)
+		not_a_solution = False
+		exit = False
+		blocks = stationary_blocks(grid)
+		new_blocks, new_grid = place_block(grid, number_of_reflect_blocks, number_of_refract_blocks, number_of_opaque_blocks)
+		blocks += new_blocks
+		first_pass = True
+		while not_a_solution == False:
+			exit = False
+			if set(positions_to_intersect).issubset(lazor_path):
+				goal_met = True	
+			for a in range(len(lazors)):
+				if first_pass == True:
+					first_pass == False
+				if overlap(lazor_path, blocks, first_pass):
+					not_a_solution = True
+				else:
+					for b in range(len(blocks)):
+				 		vertical_wall, horizontal_wall = block_wall(lazors[a].x,lazors[a].y,blocks[b].position,lazors[a].vx,lazors[a].vy)
+				 		if vertical_wall or horizontal_wall:
+				 			lazors_reflect, lazors_split, transmit, reflect = blocks[b].hit_block(lazors[a].x,lazors[a].y,lazors[a].vx,lazors[a].vy,vertical_wall,horizontal_wall)
+				 			lazors[a] = lazors_reflect	
+				 			if lazors_reflect == []:
+				 				lazors.remove(lazors[a])
+				 				exit = True
+				 				if lazors == []:
+									not_a_solution = True
+								break
+				 			if transmit == True:
+				 				lazors.append(lazors_split)
+			 				p = []
+				 			if reflect:
+				 				for b in range(len(blocks)):
+				 					x = int(lazors[a].x)
+				 					y = int(lazors[a].y)
+				 					xy = [(x,y)]
+				 					if set(xy).issubset(blocks[b].position):
+				 						p.append(1)
+				 						if len(p) > 1:
+				 							lazors.remove(lazors[a])
+				 							exit = True
+				 							if lazors == []:
+												not_a_solution = True
+				 							break
+			 					break
+	 				if exit:
+						break
+					else:
+						if lazors_in_grid(grid, lazors, a):
+							lazor_path.append((lazors[a].x, lazors[a].y))
+				 			lazors[a].move()
+				 			lazor_path.append((lazors[a].x, lazors[a].y))
+							lazor_1.append((lazors[0].x, lazors[0].y))
+							if len(lazors)>1:
+								lazor_2.append((lazors[1].x, lazors[1].y))
+						else:
+							lazor_path.append((lazors[a].x, lazors[a].y))
+							lazors.remove(lazors[a])
+							if lazors == []:
+								not_a_solution = True
+							break
+	print lazor_path
+	print goal_met
+	print new_grid
 
-if __name__ == '__main__':
-	# maps = read_map("showstopper_4.bff")
-	solve_grid("showstopper_4.bff")
+# if __name__ == '__main__':
+solve_grid("yarn_5.bff")
